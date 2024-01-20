90f86a2e7bb1c8df811fd5031e33f7eab1776c0a
sanwar hosen <ssshuvo1230@gmail.com>
2024-01-20 12:24:51 +0600

    added: -necessary pictures e.g: blog cover, author picture, user picture. -blogs.json -blogs.jsx (only fetched data) -home.jsx -header.jsx
    Deleted:
    -old unnecessary pictures

---
+++ knowledge-cafe/cafe-app/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-blue-800 font-extrabold bg-fuchsia-200 text-center p-8 ">
+                Knowledge Cafe
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ knowledge-cafe/cafe-app/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Blogs from "../Blogs/Blogs";
+import Header from "../Header/Header";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+            <Blogs></Blogs>
+        </>
+    );
+};
+
+export default Home;
